178b7f97207f5b3b93f3dd393617437b00d36a21
samih111111 <simoo111111@gmail.com>
2022-12-09 11:30:41 -0800

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - entry point of programs
+*
+*Return: return 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c<= 'z'; c++)
+		putcher(c);
+	puthar('\n');
+	return(0);
+}
